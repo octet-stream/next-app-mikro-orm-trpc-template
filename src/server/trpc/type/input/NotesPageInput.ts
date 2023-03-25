47c9a5a3fe5098ac0588e45958a8426c48ab5014
Nick K <nick.kruchinin@gmail.com>
2023-03-26 01:44:47 +0300

    Small fix

--- src/server/trpc/type/input/NotesPageInput.ts
+++ src/server/trpc/type/input/NotesPageInput.ts
@@ -4,7 +4,7 @@ import {z} from "zod"
 import {createPageInput} from "server/trpc/helper/createPageInput"
 import {NoteStatusFilterSchema} from "server/trpc/type/common/NoteStatusFilter"
 
-export const NotesPageInput = createPageInput({maxLimit: 3}, z.object({
+export const NotesPageInput = createPageInput({maxLimit: 500}, z.object({
   filter: z
     .object({status: NoteStatusFilterSchema.optional()})
     .optional()
