32bd752ff7cb93f00fadd2e117f62d8665046e5a
Nick K <nick.kruchinin@gmail.com>
2023-03-25 04:53:02 +0300

    Replace getEmptyPaths and patchStaticPaths with patchStaticParams and generateEmptyStaticParams helpers

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -4,14 +4,13 @@ import type {Metadata} from "next"
 import {Note} from "server/db/entity"
 import {getORM} from "server/lib/db/orm"
 
-import type {AFC} from "lib/type/AsyncFunctionComponent"
 import {createCaller} from "lib/trpc/server"
+import type {AFC} from "lib/type/AsyncFunctionComponent"
+import {patchStaticParams} from "lib/util/patchStaticParams"
 
 import {NoteStateContextProvider} from "context/NoteStateContext"
 
 import {NoteView} from "view/NoteView/NoteView"
-
-export const revalidate = 0
 
 interface Params {
   id: string
@@ -22,6 +21,18 @@ interface Props {
 }
 
 const getNote = createCaller((trpc, id: string) => trpc.note.getById({id}))
+
+export const generateStaticParams = patchStaticParams<Params>(async () => {
+  const orm = await getORM()
+
+  const notes = await orm.em.find(Note, {}, {
+    disableIdentityMap: true,
+    filters: false,
+    fields: ["id"]
+  })
+
+  return notes.map(({id}) => ({id}))
+})
 
 // TODO: Optimise this, maybe with cache on tRPC level
 export async function generateMetadata({params}: Props): Promise<Metadata> {
