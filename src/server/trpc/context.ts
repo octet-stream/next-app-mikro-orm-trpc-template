05eaec56a21313c0bf667b7d74d67bc2c877dc34
Nick K <nick.kruchinin@gmail.com>
2023-04-28 16:22:21 +0300

    Use App Router handlers for tRPC API

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,21 +1,20 @@
-import type {CreateNextContextOptions} from "@trpc/server/adapters/next"
-import type {NextApiRequest, NextApiResponse} from "next"
+import type {
+  FetchCreateContextFn,
+  FetchCreateContextFnOptions
+} from "@trpc/server/adapters/fetch"
 
-export type Context = object
+import type {Router} from "./router"
 
-export type SSRContext<R = any> = Context & {
-  req: NextApiRequest
-  res: NextApiResponse<R>
-}
+export interface Context { }
 
-export type GlobalContext = Context | SSRContext
+export type FetchContext = Context & FetchCreateContextFnOptions
 
-export function isSSRContext(
+export type GlobalContext = Context | FetchContext
+
+export const isFetchContext = (
   ctx: GlobalContext
-): ctx is SSRContext {
-  return !!((ctx as SSRContext)?.req && (ctx as SSRContext)?.res)
-}
+): ctx is FetchContext => !!("req" in ctx && "resHeaders" in ctx)
 
-export const createContext = (
-  ctx: CreateNextContextOptions
-): GlobalContext => isSSRContext(ctx) ? ctx : {}
+export const createContext: FetchCreateContextFn<Router> = (
+  ctx
+): GlobalContext => isFetchContext(ctx) ? ctx : {}
--- src/server/trpc/middleware/fetchContextCheck.ts
+++ src/server/trpc/middleware/fetchContextCheck.ts
@@ -1,20 +1,20 @@
 import {TRPCError} from "@trpc/server"
 
-import {isSSRContext} from "server/trpc/context"
+import {isFetchContext} from "server/trpc/context"
 import {middleware} from "server/trpc/def"
 
 /**
  * Checks whether SSRContext is present. Throws an error if is not. Narrows GlobalContext to SSRContext type.
  */
-const ssrContextCheck = middleware(({ctx, next}) => {
-  if (!isSSRContext(ctx)) {
+const fetchContextCheck = middleware(({ctx, next}) => {
+  if (!isFetchContext(ctx)) {
     throw new TRPCError({
       code: "INTERNAL_SERVER_ERROR",
-      message: "SSRContext required for this operation"
+      message: "FetchContext required for this operation"
     })
   }
 
   return next({ctx})
 })
 
-export default ssrContextCheck
+export default fetchContextCheck
