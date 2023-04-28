d6c5257fe871d644b5dad06e6a28e2da9e1a6bae
Nick K <nick.kruchinin@gmail.com>
2023-04-28 19:30:35 +0300

    Add database config validation and normalization using zod

--- src/server/lib/db/config.ts
+++ src/server/lib/db/config.ts
@@ -1,22 +1,44 @@
 import {resolve, join} from "node:path"
 
 import {defineConfig} from "@mikro-orm/mysql"
+import {z} from "zod"
 
 import {Note, Completion} from "server/db/entity"
 
 const ROOT = resolve("db")
 
-export const getConfig = async () => defineConfig({
-  implicitTransactions: true,
-  dbName: process.env.MIKRO_ORM_DB_NAME || undefined,
-  host: process.env.MIKRO_ORM_HOST || undefined,
-  port: parseInt(process.env.MIKRO_ORM_PORT || "", 10) || undefined,
-  debug: process.env.NODE_ENV === "development",
-  migrations: {
-    path: join(ROOT, "migration")
-  },
-  seeder: {
-    path: join(ROOT, "seed")
-  },
-  entities: [Note, Completion]
+const ConnectionConfig = z.object({
+  dbName: z.string().regex(/[a-z0-9-_]+/i),
+  host: z.string().optional().default("localhost"),
+  port: z.union([z.string().regex(/[0-9]+/), z.number()]).transform(port => +port),
+  debug: z.union([
+    z.literal("development"),
+    z.literal("production"),
+    z.literal("test")
+  ]).transform(env => env === "development")
 })
+
+export const getConfig = async () => {
+  const {host, port, dbName, debug} = await ConnectionConfig.parseAsync({
+    dbName: process.env.MIKRO_ORM_DB_NAME,
+    host: process.env.MIKRO_ORM_HOST,
+    port: process.env.MIKRO_ORM_PORT,
+    debug: process.env.NODE_ENV
+  })
+
+  return defineConfig({
+    dbName,
+    host,
+    port,
+    debug,
+
+    implicitTransactions: true,
+    migrations: {
+      path: join(ROOT, "migration")
+    },
+    seeder: {
+      path: join(ROOT, "seed")
+    },
+    entities: [Note, Completion]
+  })
+}
